61cf52302eb6b24035a3a9fe7ec3a36f4a9aa531
FrenchMontana10 <cecilofori1@gmail.com>
2023-07-19 13:18:07 +0000

    fourth commit

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def fibonacci_sequence(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fib_sequence = [0, 1]
+    while len(fib_sequence) < n:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    return fib_sequence
