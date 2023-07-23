c6ced85041f362f773b1333f98b312a294d1d242
FrenchMontana10 <cecilofori1@gmail.com>
2023-07-23 18:09:04 +0000

    temperature

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,11 @@
-#!/usr/bin/env python3
+def main():
+    convert_to_celcius(100)
+    convert_to_celcius(-40)
+    convert_to_celcius(-459.67)
+    convert_to_celcius(32)
+
 def convert_to_celcius(fahrenheit):
     celcius = (fahrenheit - 32) * 5 / 9
     return celcius
-result = convert_to_celcius(75)
-print(result)
+
+main()
