18f5be7b6e8a87c306a007a676fc9c54edceb7bc
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-03 18:22:25 +0000

    fourth commit

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,22 +1,55 @@
-#!/usr/bin/python3
-"""
-A class that defines a square by: (based on 2-square.py)
-"""
-
-
+"""this is class square"""
 class Square:
-    """Creates a square.
-    Private instance attributes: size
-    """
-
+    """creates a class square
+    and below is it's intialiasation"""
     def __init__(self, size=0):
-        """Initializes the data."""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+        """Initialize a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        """
         self.__size = size
 
+    @property
+    def size(self):
+        """Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
     def area(self):
-        """Return square area."""
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
         return self.__size ** 2
