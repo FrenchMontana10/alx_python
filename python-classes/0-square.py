8cb5da181e7ce164d475876764cc882ff9a3b052
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-03 18:16:31 +0000

    area of a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,26 @@
-#!/usr/bin/python3
-"""Class Square that defines a square.
-    """
+# 0-square.py
+
+"""This is a module-level documentation.
+
+This module contains a class called Square, which defines a square by its size.
+
+"""
+
 class Square:
-    """Class Square that defines a square.
+    """Represents a square.
+
+    This class defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
     """
-    pass
+
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        """
+        self.__size = size
