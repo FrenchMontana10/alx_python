087095973c715f36dd18e8ab442755f92a948a28
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-10 18:05:52 +0000

    fifth commit

---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Search API """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+    
+    response = requests.post(url, data=payload)
+    
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
