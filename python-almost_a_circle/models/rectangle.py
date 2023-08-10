bb004f09582ae5fc626356bd9df2bb24a4ce2974
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-10 14:24:26 +0000

    rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Doc """
+""" Combined Code """
 
 class Base:
     """This class will be the base of all other classes in the project.
@@ -44,86 +44,32 @@ class Rectangle(Base):
         self.x = x
         self.y = y
         
-    @property
-    def width(self):
-        return self.__width
+    # ... (previous methods and properties)
     
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
+    def update(self, *args):
+        """Update attributes based on arguments."""
+        attributes = ["id", "width", "height", "x", "y"]
+        for index, value in enumerate(args):
+            if index < len(attributes):
+                setattr(self, attributes[index], value)
     
-    @property
-    def height(self):
-        return self.__height
-    
-    @height.setter
-    def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-    
-    @property
-    def x(self):
-        return self.__x
-    
-    @x.setter
-    def x(self, value):
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-    
-    @property
-    def y(self):
-        return self.__y
-    
-    @y.setter
-    def y(self, value):
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-    
-    def area(self):
-        """Calculate and return the area of the Rectangle."""
-        return self.__width * self.__height
-    
-    def display(self):
-        """Display the Rectangle using '#' characters, considering x and y."""
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-    
-    def __str__(self):
-        """Return a string representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y, self.__width, self.__height)
+    # ... (previous methods and properties)
 
 if __name__ == "__main__":
-
     r1 = Rectangle(10, 10, 10, 10)
     print(r1)
 
-    r1.update(89)
+    r1.update(12)
     print(r1)
 
-    r1.update(89, 2)
+    r1.update(12, 4)
     print(r1)
 
-    r1.update(89, 2, 3)
+    r1.update(12, 4, 3)
     print(r1)
 
-    r1.update(89, 2, 3, 4)
+    r1.update(12, 4, 3, 2)
     print(r1)
 
-    r1.update(89, 2, 3, 4, 5)
+    r1.update(12, 4, 3, 2, 1)
     print(r1)
