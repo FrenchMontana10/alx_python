211ad04aa8eb21469bd66c384796fa05269e6d47
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-10 02:49:06 +0000

    square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,116 @@
+#!/usr/bin/python3
+""" 9-main """
+class Base:
+    """This class will be the base of all other classes in the project.
+    
+    It manages the id attribute for all future classes and avoids duplicating code.
+    """
+
+    # Private class attribute to keep track of the number of objects created
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Constructor for the Base class.
+
+        Args:
+            id (int, optional): The ID for the instance. Defaults to None.
+        """
+        if id is not None:
+            # If an id is provided, assign it to the instance's id attribute
+            self.id = id
+        else:
+            # If no id is provided, increment __nb_objects and assign the new value to id
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+class Rectangle(Base):
+    """This class inherits from the Base class and represents a rectangle.
+    """
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor for the Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int, optional): The x-coordinate. Defaults to 0.
+            y (int, optional): The y-coordinate. Defaults to 0.
+            id (int, optional): The ID for the instance. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+    
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+    
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+class Square(Rectangle):
+    """This class inherits from the Rectangle class and represents a square.
+    """
+    
+    def __init__(self, size, x=0, y=0, id=None):
+        """Constructor for the Square class.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate. Defaults to 0.
+            y (int, optional): The y-coordinate. Defaults to 0.
+            id (int, optional): The ID for the instance. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """Overloaded __str__ method to represent the Square object as a string."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+if __name__ == "__main__":
+
+    s1 = Square(5)
+    print(s1)
+    print(s1.area())
+    s1.display()
+
+    print("---")
+
+    s2 = Square(2, 2)
+    print(s2)
+    print(s2.area())
+    s2.display()
+
+    print("---")
+
+    s3 = Square(3, 1, 3)
+    print(s3)
+    print(s3.area())
+    s3.display()
