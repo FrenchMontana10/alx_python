6c7240262dc951f5df44e9b6fa1e933c2117ac08
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-11 00:24:01 +0000

    last commit

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,38 +1,21 @@
-"""this is my module"""
-class metaclassbase(type):
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+#!/usr/bin/python3
+""" Rectangle module """
+Rectangle = __import__('7-rectangle').Rectangle
 
-"""this is my class"""
-class BaseGeometry(metaclass=metaclassbase):
-    """This is a place holder for this empty class"""
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
-    def area(self):
-        raise Exception("area() is not implemented")
-    
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f'{name} must be an integer')
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+class Square(Rectangle):
+    """ Square class """
 
-"""this is the rectangle class"""
-class Rectangle(BaseGeometry):
-    """This is a class that inherits from the BaseGeometry Class"""
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        
-    """this is the area"""
-    def area(self):
-        return self.__width * self.__height
-    
+    def __init__(self, size):
+        """ Initialize instance attributes """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
     def __str__(self):
-        return f"[Rectangle] {self.__width}/{self.__height}"
+        """ Return a string representation of the object """
+        return "[Square] {}/{}".format(self.__size, self.__size)
 
-    def __repr__(self):
-        return f"Rectangle({self.__width}, {self.__height})"
+if __name__ == "__main__":
+    s = Square(13)
+    print(s)
+    print(s.area())
