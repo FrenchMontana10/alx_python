6add7dd14a3d63676f0ac5cd74661561d1b09fba
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-11 00:06:40 +0000

    sixth commit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,82 +1,28 @@
 #!/usr/bin/python3
-"""Define a rectangle"""
+""" Base Geometry module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
 
-class Rectangle:
-    """Creates a rectangle."""
+    def __init__(self, width, height):
+        """ Initialize instance attributes """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
 
-    number_of_instances = 0
+if __name__ == "__main__":
+    r = Rectangle(3, 5)
+    print(r)
+    print(dir(r))
 
-    def __init__(self, width=0, height=0):
-        """Initialize data."""
-        self.width = width
-        self.height = height
-        Rectangle.number_of_instances += 1
+    try:
+        print("Rectangle: {} - {}".format(r.width, r.height))
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
 
-    def __str__(self):
-        """Returns the rectangle with the character #"""
-        if self.__width == 0 or self.__height == 0:
-            return ''
-        rectangle = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rectangle += '#'
-            rectangle += '\n'
-        return rectangle[:-1]
-
-    def __repr__(self):
-        """Return a string representation of the rectangle to be able
-        to recreate a new instance by using eval() (see example below)
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """Print the message Bye rectangle...
-        (... being 3 dots not ellipsis) when an
-        instance of Rectangle is deleted"""
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
-
-    @property
-    def width(self):
-        """Retrieves the width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of a Rectangle instance
-        Args:
-            value: value of width, must be >=0
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves the height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Sets the height of a Rectangle instance
-        Args:
-            value: value of height, must be >=0
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """returns the rectangle area."""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """Returns the rectangle perimeter"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+    try:
+        r2 = Rectangle(4, True)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
