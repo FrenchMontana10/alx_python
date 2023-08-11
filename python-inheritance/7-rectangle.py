188b1b8f56728269f3a333184399199d5eefcdf7
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-11 00:18:29 +0000

    update

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,83 +1,42 @@
-#!/usr/bin/python3
-"""Define a rectangle"""
+"""this is an empty class"""
+class metaclassbase(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    """this is my class"""
+class BaseGeometry(metaclass = metaclassbase):
+    """this is a place holder for this empty class"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
 
 
-class Rectangle:
-    """Creates a rectangle."""
 
-    number_of_instances = 0
-    print_symbol = '#'
+class Rectangle(BaseGeometry):
+    """This is a class that inherits from the BaseGeometry Class"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)  # Corrected parameter name
 
-    def __init__(self, width=0, height=0):
-        """Initialize data."""
-        self.width = width
-        self.height = height
-        Rectangle.number_of_instances += 1
-
+        
+        
+    def area(self):
+        return self.__width * self.__height
+    
     def __str__(self):
-        """Returns the rectangle with the character #"""
-        if self.__width == 0 or self.__height == 0:
-            return ''
-        rectangle = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rectangle += str(self.print_symbol)
-            rectangle += '\n'
-        return rectangle[:-1]
+        return f"[Rectangle] {self.__width}/{self.__height}"
 
     def __repr__(self):
-        """Return a string representation of the rectangle to be able
-        to recreate a new instance by using eval() (see example below)
-        """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """Print the message Bye rectangle...
-        (... being 3 dots not ellipsis) when an
-        instance of Rectangle is deleted"""
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
-
-    @property
-    def width(self):
-        """Retrieves the width."""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """Sets the width of a Rectangle instance
-        Args:
-            value: value of width, must be >=0
-        """
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Retrieves the height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Sets the height of a Rectangle instance
-        Args:
-            value: value of height, must be >=0
-        """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        """returns the rectangle area."""
-        return self.__width * self.__height
-
-    def perimeter(self):
-        """Returns the rectangle perimeter"""
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+        return f"Rectangle({self.__width}, {self.__height})"
