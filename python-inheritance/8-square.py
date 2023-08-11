e14a151fc405b751c795c4e4d2914b621bf55977
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-11 00:20:10 +0000

    last commit

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,9 +1,38 @@
-#!/usr/bin/python3
-"""Class Square that defines a square.
-    """
+"""this is my module"""
+class metaclassbase(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+"""this is my class"""
+class BaseGeometry(metaclass=metaclassbase):
+    """This is a place holder for this empty class"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
-class Square:
-    """Class Square that defines a square.
-    """
-    pass
+"""this is the rectangle class"""
+class Rectangle(BaseGeometry):
+    """This is a class that inherits from the BaseGeometry Class"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        
+    """this is the area"""
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
