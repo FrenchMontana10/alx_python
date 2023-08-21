56b4b6147d73400e5d458b2b6354ae472a117c45
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-21 12:56:21 +0000

    second commit

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,15 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Create the database and table if they don't exist
     db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
+    cur.execute("CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa")
+    cur.execute("USE hbtn_0e_0_usa")
+    cur.execute("CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id))")
+    
+    # Insert some data into the states table
+    cur.execute("INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas'), ('New York'), ('Nevada')")
     
     # Execute the SQL query to select states starting with 'N'
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
