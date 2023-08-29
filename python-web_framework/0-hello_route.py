db4e72f9fe7da10ff7d4895d9426f89a8727ed29
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-29 02:19:12 +0000

    update

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,12 +1,14 @@
-!#/usr/bin/python3
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
 
-# Define a route for the root URL with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.errorhandler(404)
+def not_found(error):
+    return 'Not Found', 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
