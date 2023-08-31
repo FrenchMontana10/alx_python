5ebd966d331311e7933c296946627df077ec0f0e
FrenchMontana10 <cecilofori1@gmail.com>
2023-08-31 20:55:57 +0000

    second commit

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,18 +1,21 @@
-from flask import Flask, abort
+"""here we import the flask module"""
+from flask import Flask
 
+"""we intialise the Flask module"""
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return 'HBNB'
+"""we declare the route of the resource"""
+@app.route("/", strict_slashes = False)
+def main():
+    """here is the function that executes when that route is hit"""
+    return "Hello HBNB!"
 
-@app.errorhandler(404)
-def not_found(error):
-    return 'Not Found', 404
+@app.route("/hbnb", strict_slashes = False)
+def first():
+    """this is the function"""
+    return "HBNB"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port = 5000)
+    
