e7e8755df81b10f8fa8282a9c6b76049df6e8938
FrenchMontana10 <cecilofori1@gmail.com>
2023-10-06 17:17:26 +0000

    update

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,14 +5,16 @@ from sys import argv
 if __name__ == "__main__":
     user_id = argv[1]
 
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    response = requests.get(url)
-    user = response.json()
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    user = user_response.json()
+    username = user['username']
 
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    response = requests.get(url)
-    tasks = response.json()
+    tasks_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+    tasks = tasks_response.json()
 
     with open(f"{user_id}.csv", mode='w', newline='') as file:
         writer = csv.writer(file)
-        writer.writerows([[user_id, user['username'], str(task['completed']), task['title']] for task in tasks])
+        writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for task in tasks:
+            writer.writerow([user_id, username, str(task['completed']), task['title']])
